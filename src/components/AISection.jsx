d40b7d8f39dd16afd9f44cca1974c90a26d8a8b7
Payal091999 <payalbera091999@gmail.com>
2025-06-05 13:42:55 +0530

    Update API endpoint to use relative path

--- src/components/AISection.jsx
+++ src/components/AISection.jsx
@@ -16,7 +16,7 @@ export default function AISection() {
     setUserInput('');
 
     try {
-      const response = await axios.post('http://localhost:5000/api/chat', {
+      const response = await axios.post('/api/chat', {
         message: userInput,
       });
 
