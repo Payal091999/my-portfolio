0999a56f9c4874d1c080ce593beca3cd63ad6f24
Payal091999 <payalbera091999@gmail.com>
2025-06-05 22:01:20 +0530

    Update emailjs package to @emailjs/browser

--- src/pages/Admin.jsx
+++ src/pages/Admin.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import emailjs from 'emailjs-com';
+import emailjs from '@emailjs/browser';
 
 export default function AdminPanel() {
   const [email, setEmail] = useState('');
